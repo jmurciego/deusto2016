898dd5f0d9e0cd8763109f1b29b2bacdc696f1e7
jmurciego <jmurciego@picolo.local>
2016-12-03 17:33:40 +0100

    Correccion ejemplo CGLib

--- CGLib/src/main/java/es/deusto/masf/cglib/Main.java
+++ CGLib/src/main/java/es/deusto/masf/cglib/Main.java
@@ -4,11 +4,13 @@ public class Main {
 
     public static void main(String args[]) {
         ClaseSaludo saludador = new ClaseSaludo();
-        System.out.println(saludador);
+        System.out.println(saludador.saluda("Javi"));
+        
+        System.out.println(saludador.getClass().getName());
+        
         ClaseSaludo saludadorConCGLIB = (ClaseSaludo) ModificadorByteCodeConCGLibTrazas.createWrapper(saludador, ClaseSaludo.class);
         System.out.println(saludadorConCGLIB.saluda("Javi"));
         System.out.println(saludadorConCGLIB.getClass().getName());
-        System.out.println(saludador);
     }
 
 }
